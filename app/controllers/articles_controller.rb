8bc4095a528ccf7f2889debb6f8938308794e69e
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-21 15:07:46 -0500

    Dry up articles controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,16 @@
 class ArticlesController < ApplicationController
-  
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+
+
+
+
+
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
   
   def new
@@ -13,11 +18,10 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was created successfully."
       redirect_to @article 
@@ -27,8 +31,8 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    
+    if @article.update(article_params)
       flash[:notice] = "Article was created successfully."
       redirect_to @article
     else
@@ -38,12 +42,19 @@ class ArticlesController < ApplicationController
 
 
   def destroy
-    @article = Article.find_by(id: params[:id])
     @article.destroy
     flash[:notice] = "Article was removed successfully."
     redirect_to articles_path
   end
 
+  private
 
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
 end
